5f4fb262e8e7af6b8db8a802449d31fbb8300a1e
tclewis91 <tclewis91@gmail.com>
2015-06-01 18:04:11 -0500

    element methods

--- lib/requests.rb
+++ lib/requests.rb
@@ -1,14 +1,18 @@
-class Requests
+class Request
   def initialize
   end
 
-def hwk_methods
-  if params[:resource] == 'user'
-    @user = User.all
-    @user.each do |element|
-    puts "#{element.first_name}"
-    puts "#{element.last_name}"
-    puts "#{element.age}" #does this need to be in a string?
+  def hwk_methods(params)
+    if params[:resource] == 'users'  #but there's no resource...so fuck
+      user = User.all
+      user.each do |element|
+        puts "#{element.first_name}"
+        puts "#{element.last_name}"
+        puts "#{element.age}" #does this need to be in a string?
+      end
+    else
+      puts "hey"
     end
   end
 end
+
