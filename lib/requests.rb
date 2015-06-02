d4f65d5fdc49e7c44e52db5e515391dfa2e977e9
tclewis91 <tclewis91@gmail.com>
2015-06-01 22:53:45 -0500

    running methods

--- lib/requests.rb
+++ lib/requests.rb
@@ -1,18 +1,19 @@
 class Request
+
   def initialize
     @num = [1..4]
   end
 
   def element_methods(params)
-    # if params[:resource] == 'users'
-    #   user = User.all
-    #   user.each do |element|
-    #     puts "#{element.first_name}"
-    #     puts "#{element.last_name}"
-    #     puts "#{element.age}" #does this need to be in a string?
-    #   end
-      #end
-    #end
+    if params[:resource] == 'users'
+      user = User.all
+      user.each do |element|
+        puts "#{element.first_name}"
+        puts "#{element.last_name}"
+        puts "#{element.age}" #does this need to be in a string?
+      end
+    end
+
     if params[:id] != nil
       individual_id = User.find(@num)
       individual_id.each do |element|
@@ -20,8 +21,9 @@ class Request
         puts "#{element.last_name}"
         puts "#{element.age}"
       end
+    else
+      puts "Error 404 Page Not Found"
     end
   end
 end
-
 
